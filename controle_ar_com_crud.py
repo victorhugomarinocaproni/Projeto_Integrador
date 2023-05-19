06f09364f44dd27ef190f32fef7aeafa6906a9dc
Victor Caproni <victorcaproni04@gmail.com>
2023-05-19 17:23:51 -0300

    Versão v0.1.3 do projeto com Crud - Inserir e Listar já estão funcioando perfeitamente

--- controle_ar_com_crud.py
+++ controle_ar_com_crud.py
@@ -9,120 +9,15 @@ obtemConexaoComMySQL.conexao=None
 
 resposta_usuario = ''
 resposta_de_saida = ''
+numero_da_linha = 1
 
 resultado = []
 resultado_aproximado = []
 
-lista_dicionarios = [{
-
-    'elemento' : 'MCP10',
-    'nome' : 'select MCP10 from elementos',
-    'valor1' : 41,
-    'valor2' : 81,
-    'valor3' : 121,
-    'concentracao1' : 50,
-    'concentracao2' : 100, 
-    'concentracao3' : 150,
-    'concentracao4' : 250,
-    'concentracao5' : 3000,
-    'calculo_1' : 0.8, # --> 40/50
-    'calculo_2' : 0.78, # -->(80-41)/(100-50)
-    'calculo_3' : 0.78, # --> (120-81)/(150-100)
-    'calculo_4' : 0.79, # --> (200-121)/(250-150)
-    'calculo_5' : 0.029, # --> (200-121)/(3000-250)
-},
-
-{
-    'elemento' : 'MP2_5',
-    'nome' : 'select MP2_5 from elementos',
-    'valor1' : 41,
-    'valor2' : 81,
-    'valor3' : 121,
-    'concentracao1' : 25,
-    'concentracao2' : 50, 
-    'concentracao3' : 75,
-    'concentracao4' : 125,
-    'concentracao5' : 3000,
-    'calculo_1' : 1.6, # --> 40/25
-    'calculo_2' : 1.56, # --> (80-41)/(50-25)
-    'calculo_3' : 1.56, # --> (120-81)/(75-50)
-    'calculo_4' : 1.58, # --> (200-121)/(125-75)
-    'calculo_5' : 0.02747826, # --> (200-121)/(3000-125)
-},
-
-{
-    'elemento' : 'O3',
-    'nome' : 'select O3 from elementos',
-    'valor1' : 41,
-    'valor2' : 81,
-    'valor3' : 121,
-    'concentracao1' : 100,
-    'concentracao2' : 130, 
-    'concentracao3' : 160,
-    'concentracao4' : 200,
-    'concentracao5' : 3000,
-    'calculo_1' : 0.4, # --> 40/100
-    'calculo_2' : 1.3, # --> (80-41)/(130-100)
-    'calculo_3' : 1.3, # --> (120-81)/(160-130)
-    'calculo_4' : 1.975, # --> (200-121)/(200-160)
-    'calculo_5' : 0.00282143, # --> (200-121)/(3000-200)
-},
-{
-    'elemento' : 'MCO',
-    'nome' : 'select MCO from elementos',
-    'valor1' : 41,
-    'valor2' : 81,
-    'valor3' : 121,
-    'concentracao1' : 9,
-    'concentracao2' : 11, 
-    'concentracao3' : 13,
-    'concentracao4' : 15,
-    'concentracao5' : 3000,
-    'calculo_1' : 4.44444444445, # --> 40/9
-    'calculo_2' : 19.5, # --> (80-41)/(11-9)
-    'calculo_3' : 19.5, # --> (120-81)/(13-11)
-    'calculo_4' : 39.5, # --> (200-121)/(15-13)
-    'calculo_5' : 0.0264657, # --> (200-121)/(3000-15)
-},
-{
-    'elemento' : 'NO2',
-    'nome' : 'select NO2 from elementos',
-    'valor1' : 41,
-    'valor2' : 81,
-    'valor3' : 121,
-    'concentracao1' : 200,
-    'concentracao2' : 240, 
-    'concentracao3' : 320,
-    'concentracao4' : 1130,
-    'concentracao5' : 3000,
-    'calculo_1' : 0.2, # --> 40/200
-    'calculo_2' : 0.975, # --> (80-41)/(240-200)
-    'calculo_3' : 0.4875, # --> (120-81)/(320-240)
-    'calculo_4' : 0.97531, # --> (200-121)/(1130-320)
-    'calculo_5' : 0.042246, # --> (200-121)/(3000-1130)
-},
-{
-    'elemento' : 'SO2',
-    'nome' : 'select SO2 from elementos',
-    'valor1' : 41,
-    'valor2' : 81,
-    'valor3' : 121,
-    'concentracao1' : 20,
-    'concentracao2' : 40, 
-    'concentracao3' : 365,
-    'concentracao4' : 800,
-    'concentracao5' : 3000,
-    'calculo_1' : 2, # --> 40/20
-    'calculo_2' : 1.95, # --> (80-41)/(40-20)
-    'calculo_3' : 0.12, # --> (120-81)/(365-40)
-    'calculo_4' : 0.18161, # --> (200-121)/(800-365)
-    'calculo_5' : 0.03591, # --> (200-121)/(3000-800)
-},
-]
 
 def formula_calculo(lista_de_dicionarios):
 
-    for dicionario in lista_dicionarios:
+    for dicionario in lista_de_dicionarios:
         dicionario = dicionario
 
         conexao=obtemConexaoComMySQL('localhost','root','1234','projeto_integrador')
@@ -224,7 +119,7 @@ def verifica_pior_indice(lista_resultado_aproximado):
         print('Qualidade N5 - PÉSSIMA')
 
 
-def create():
+def create(numero_linha):
 
     conexao=obtemConexaoComMySQL('localhost','root','1234','projeto_integrador')
     cursor=conexao.cursor()
@@ -242,7 +137,7 @@ def create():
 
     mcp10,mp2_5,o3,mco,no2,so2 = lista_valores_digitados
 
-    comando = f'INSERT INTO elementos (MCP10,MP2_5,O3,MCO,NO2,SO2) VALUES {mcp10,mp2_5,o3,mco,no2,so2};'
+    comando = f'INSERT INTO elementos (nmr_linha,MCP10,MP2_5,O3,MCO,NO2,SO2) VALUES {numero_linha,mcp10,mp2_5,o3,mco,no2,so2};'
     cursor.execute(comando)
     conexao.commit()
 
@@ -274,60 +169,198 @@ def delete():
     conexao=obtemConexaoComMySQL('localhost','root','1234','projeto_integrador')
     cursor=conexao.cursor()
 
-    comando = f'DELETE FROM ELEMENTOS WHERE MCP10 > -1 OR MP2_5 > -1 OR O3 > -1 OR MCO > -1 OR NO2 > -1 OR SO2 > -1'
+    comando = f'DELETE FROM ELEMENTOS WHERE nmr_linha > -1 OR MCP10 > -1 OR MP2_5 > -1 OR O3 > -1 OR MCO > -1 OR NO2 > -1 OR SO2 > -1'
     cursor.execute(comando)
     conexao.commit()
 
 
+delete()
+passar_comeco = False
 while True:
 
-    delete()
-    print('Insira as concentrações obtidas em campo para que o programa calcula a qualidade do ar.')
-    print()
+    resultado = []
+    resultado_aproximado = []
 
-    create()
-    formula_calculo(lista_dicionarios)
-    verifica_pior_indice(resultado)
+    lista_dicionarios = [{
+
+        'elemento' : 'MCP10',
+        'nome' : f'select MCP10 from elementos where nmr_linha="{numero_da_linha}"',
+        'valor1' : 41,
+        'valor2' : 81,
+        'valor3' : 121,
+        'concentracao1' : 50,
+        'concentracao2' : 100, 
+        'concentracao3' : 150,
+        'concentracao4' : 250,
+        'concentracao5' : 3000,
+        'calculo_1' : 0.8, # --> 40/50
+        'calculo_2' : 0.78, # -->(80-41)/(100-50)
+        'calculo_3' : 0.78, # --> (120-81)/(150-100)
+        'calculo_4' : 0.79, # --> (200-121)/(250-150)
+        'calculo_5' : 0.029, # --> (200-121)/(3000-250)
+        },
+
+        {
+        'elemento' : 'MP2_5',
+        'nome' : f'select MP2_5 from elementos where nmr_linha="{numero_da_linha}"',
+        'valor1' : 41,
+        'valor2' : 81,
+        'valor3' : 121,
+        'concentracao1' : 25,
+        'concentracao2' : 50, 
+        'concentracao3' : 75,
+        'concentracao4' : 125,
+        'concentracao5' : 3000,
+        'calculo_1' : 1.6, # --> 40/25
+        'calculo_2' : 1.56, # --> (80-41)/(50-25)
+        'calculo_3' : 1.56, # --> (120-81)/(75-50)
+        'calculo_4' : 1.58, # --> (200-121)/(125-75)
+        'calculo_5' : 0.02747826, # --> (200-121)/(3000-125)
+        },
+
+        {
+        'elemento' : 'O3',
+        'nome' : f'select O3 from elementos where nmr_linha="{numero_da_linha}"',
+        'valor1' : 41,
+        'valor2' : 81,
+        'valor3' : 121,
+        'concentracao1' : 100,
+        'concentracao2' : 130, 
+        'concentracao3' : 160,
+        'concentracao4' : 200,
+        'concentracao5' : 3000,
+        'calculo_1' : 0.4, # --> 40/100
+        'calculo_2' : 1.3, # --> (80-41)/(130-100)
+        'calculo_3' : 1.3, # --> (120-81)/(160-130)
+        'calculo_4' : 1.975, # --> (200-121)/(200-160)
+        'calculo_5' : 0.00282143, # --> (200-121)/(3000-200)
+        },
+        {
+        'elemento' : 'MCO',
+        'nome' : f'select MCO from elementos where nmr_linha="{numero_da_linha}"',
+        'valor1' : 41,
+        'valor2' : 81,
+        'valor3' : 121,
+        'concentracao1' : 9,
+        'concentracao2' : 11, 
+        'concentracao3' : 13,
+        'concentracao4' : 15,
+        'concentracao5' : 3000,
+        'calculo_1' : 4.44444444445, # --> 40/9
+        'calculo_2' : 19.5, # --> (80-41)/(11-9)
+        'calculo_3' : 19.5, # --> (120-81)/(13-11)
+        'calculo_4' : 39.5, # --> (200-121)/(15-13)
+        'calculo_5' : 0.0264657, # --> (200-121)/(3000-15)
+        },
+        {
+        'elemento' : 'NO2',
+        'nome' : f'select NO2 from elementos where nmr_linha="{numero_da_linha}"',
+        'valor1' : 41,
+        'valor2' : 81,
+        'valor3' : 121,
+        'concentracao1' : 200,
+        'concentracao2' : 240, 
+        'concentracao3' : 320,
+        'concentracao4' : 1130,
+        'concentracao5' : 3000,
+        'calculo_1' : 0.2, # --> 40/200
+        'calculo_2' : 0.975, # --> (80-41)/(240-200)
+        'calculo_3' : 0.4875, # --> (120-81)/(320-240)
+        'calculo_4' : 0.97531, # --> (200-121)/(1130-320)
+        'calculo_5' : 0.042246, # --> (200-121)/(3000-1130)
+        },
+        {
+        'elemento' : 'SO2',
+        'nome' : f'select SO2 from elementos where nmr_linha="{numero_da_linha}"',
+        'valor1' : 41,
+        'valor2' : 81,
+        'valor3' : 121,
+        'concentracao1' : 20,
+        'concentracao2' : 40, 
+        'concentracao3' : 365,
+        'concentracao4' : 800,
+        'concentracao5' : 3000,
+        'calculo_1' : 2, # --> 40/20
+        'calculo_2' : 1.95, # --> (80-41)/(40-20)
+        'calculo_3' : 0.12, # --> (120-81)/(365-40)
+        'calculo_4' : 0.18161, # --> (200-121)/(800-365)
+        'calculo_5' : 0.03591, # --> (200-121)/(3000-800)
+        },
+    ]
+
+    if passar_comeco:
+        pass
+    else:
+        print('Insira as concentrações obtidas em campo para que o programa calcula a qualidade do ar.')
+        print()
+
+        create(numero_da_linha)
+        formula_calculo(lista_dicionarios)
+        verifica_pior_indice(resultado)
 
 
     while True:
 
+        passar_comeco = False
         print()
-        resposta_usuario = input('Você deseja realizar alguma ação? [listar] ou [atualizar] ou [inserir]: ').lower()
+        resposta_usuario = input('Você deseja realizar alguma ação? [listar] ou [atualizar] ou [inserir] ou [deletar] ou [sair]: ').lower()
         print()
 
-        if resposta_usuario in ['listar','atualizar','inserir']:
+        if resposta_usuario in ['listar','atualizar','inserir','deletar','sair']:
             break
         else:
             print('Por favor, digite uma das opções corretamente')
 
     if resposta_usuario == 'listar':
         read()
+        passar_comeco = True
 
     elif resposta_usuario == 'atualizar':
+        read()
         update()
         resultado = []
         resultado_aproximado = []
         formula_calculo(lista_dicionarios)
         verifica_pior_indice(resultado)
-        
+        passar_comeco = True
 
-    else:
-        delete()
+        """
+            A fazer:
+                Arrumar a maneira como a lista aparece para o usuário, e pedir pra ele escolher qual linha quer alterar.
+                Além disso, após pedir a linha, o programa deve pedir para o usuário qual elemento ele deseja atualizar, e limitar
+                esta mudança apenas na linha escolhida. (limitar usando o WHERE na query).
+        """
+        
+    elif resposta_usuario == 'inserir':
+        numero_da_linha +=1
         continue
 
-    while True:
-
-        print()
-        resposta_de_saida = input('Você deseja sair do programa? [sim] ou [nao]: ').lower()
-
-        if resposta_de_saida in ['sim','s','nao','n']:
-            break
-        else:
-            print('Por favor, digite [sim] ou [nao]')
-
-    if resposta_de_saida in ['sim','s']:
+    elif resposta_usuario == 'sair':
         break
 
-print()
+    elif resposta_usuario == 'deletar':
+
+        read()
+
+        while True:
+        
+            try: 
+                linha_a_deletar = int(input('Digite a linha que você deseja deletar: '))
+                break
+
+            except ValueError:
+                print('Por favor, digite o número da linha que aparece como sendo o primeiro número de cada lista.')
+
+        """
+            A fazer:
+                Arrumar a query para deletar apenas a linha desejada pelo usuário. Além disso, após excluir uma linha, deverá ser feita uma query
+                no banco para atualizar a outra linha já existente e diminuir seu número de linha.
+                Arrumar a maneira como as linhas aparecem para o usuário para facilitar a interpretação de o que é cada coisa que aparece.
+        """
+
+
+    if resposta_usuario == 'sair':
+        break
+
+
 print('Obrigado por utilizar nosso programa!')
